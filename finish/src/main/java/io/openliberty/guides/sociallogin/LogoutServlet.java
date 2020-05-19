ca2a7566031e95a79f89ba196a579687cf28e993
k-rtik <kartikrajkanna@gmail.com>
2020-05-19 10:33:52 -0400

    env var for git config; update next steps

--- finish/src/main/java/io/openliberty/guides/sociallogin/LogoutServlet.java
+++ finish/src/main/java/io/openliberty/guides/sociallogin/LogoutServlet.java
@@ -1,7 +1,9 @@
 package io.openliberty.guides.sociallogin;
 
 import com.ibm.websphere.security.social.UserProfileManager;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.HttpConstraint;
 import javax.servlet.annotation.ServletSecurity;
@@ -29,73 +31,52 @@ import java.util.Map;
 // tag::LogoutServlet[]
 public class LogoutServlet extends HttpServlet {
 
+    @Inject
+    @ConfigProperty(name = "github.client.id")
+    private String clientId;
+
+    @Inject
+    @ConfigProperty(name = "github.client.secret")
+    private String clientSecret;
+
     // tag::handlePost[]
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
 
-        // GitHub endpoint and required credentials
-        // tag::GitHubInfo[]
-        // tag::clientId[]
-        final String clientId = "[github-client-id]";
-        // end::clientId[]
-        // tag::clientSecret[]
-        final String clientSecret = "[github-client-secret]";
-        // end::clientSecret[]
-        // tag::unauthorizeUrl[]
         final String unauthorizeUrl = "https://api.github.com/" +
                 "applications/{client_id}/grant";
-        // end::unauthorizeUrl[]
-        // end::GitHubInfo[]
 
         // Get access token
-        // tag::accessToken[]
         String accessToken = UserProfileManager
                 .getUserProfile()
                 .getAccessToken();
-        // end::accessToken[]
 
 
-        // tag::requestBody[]
         Map<String, String> requestBody = new HashMap<>();
         requestBody.put("access_token", accessToken);
-        // end::requestBody[]
 
-        // tag::encodeAuth[]
         String auth = clientId + ":" + clientSecret;
         byte[] encodedAuthStream = Base64
                 .getEncoder()
                 .encode(auth.getBytes(StandardCharsets.ISO_8859_1));
         String encodedAuth = new String(encodedAuthStream);
-        // end::encodeAuth[]
 
         // Add headers and body to DELETE request to delete OAuth2 grant
-        // tag::revokePermissions[]
         Response logoutResponse = ClientBuilder
                 .newClient()
-                // tag::target[]
                 .target(unauthorizeUrl)
                 .resolveTemplate("client_id", clientId)
-                // end::target[]
                 .request()
-                // tag::authHeader[]
                 .header("Authorization", "Basic " + encodedAuth)
-                // end::authHeader[]
-                // tag::accessTokenBody[]
                 .method("DELETE", Entity.json(requestBody));
-                // end::accessTokenBody[]
 
         if (logoutResponse.getStatus() != 204) {
             throw new ServletException("Could not delete OAuth2 application grant");
         }
-        // end::revokePermissions[]
 
-        // tag::logout[]
         request.logout();
-        // end::logout[]
-        // tag::redirect[]
         response.sendRedirect("hello.html");
-        // end::redirect[]
     }
     // end::handlePost[]
 }
