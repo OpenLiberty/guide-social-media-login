40fb5d64e2b9cbc93935fabd2d54f45d577e859e
Gilbert Kwan <gkwan@ca.ibm.com>
2022-02-24 12:41:18 -0500

    update versions

--- finish/src/main/java/io/openliberty/guides/sociallogin/logout/LogoutHandler.java
+++ finish/src/main/java/io/openliberty/guides/sociallogin/logout/LogoutHandler.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2020 IBM Corporation and others.
+ * Copyright (c) 2020, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,8 +14,8 @@ package io.openliberty.guides.sociallogin.logout;
 
 import com.ibm.websphere.security.social.UserProfileManager;
 
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
 
 @RequestScoped
 public class LogoutHandler {
