11fefb860d9ca1196b07687bdb7d7db92f4032c7
t27gupta <tushar.gupta@uwaterloo.ca>
2022-07-19 12:56:32 -0400

    contributing.md

--- start/src/main/java/io/openliberty/guides/sociallogin/logout/GitHubLogout.java
+++ start/src/main/java/io/openliberty/guides/sociallogin/logout/GitHubLogout.java
@@ -1,24 +1,53 @@
-// tag::copyright[]
-/*******************************************************************************
- * Copyright (c) 2020, 2022 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - Initial implementation
- *******************************************************************************/
-// end::copyright[]
 package io.openliberty.guides.sociallogin.logout;
 
+import com.ibm.websphere.security.social.UserProfileManager;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.Response;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequestScoped
 public class GitHubLogout implements ILogout {
 
+    @Inject
+    @ConfigProperty(name = "github.client.id")
+    private String clientId;
+
+    @Inject
+    @ConfigProperty(name = "github.client.secret")
+    private String clientSecret;
+
     public Response logout() {
-        return null;
+
+        final String unauthorizeUrl = "https://api.github.com/"
+                + "applications/{client_id}/grant";
+
+        String accessToken = UserProfileManager
+                .getUserProfile()
+                .getAccessToken();
+
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("access_token", accessToken);
+
+        String auth = clientId + ":" + clientSecret;
+        byte[] encodedAuthStream = Base64
+                .getEncoder()
+                .encode(auth.getBytes(StandardCharsets.ISO_8859_1));
+        String encodedAuth = new String(encodedAuthStream);
+
+        return ClientBuilder
+                .newClient()
+                .target(unauthorizeUrl)
+                .resolveTemplate("client_id", clientId)
+                .request()
+                .header("Authorization", "Basic " + encodedAuth)
+                .method("DELETE", Entity.json(requestBody));
     }
 }
